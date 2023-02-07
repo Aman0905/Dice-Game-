e6b4edf6475750f6948bfa0b763f4720b7e2b986
AMAN KUMAR GUPTA <akg42488@gmail.com>
2023-02-07 17:30:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var randomNum1 = Math.floor(Math.random() * 6) + 1;
+function myFunction() {
+  var randomNum1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImg = "images/dice" + randomNum1 + ".png";
+  var randomImg = "images/dice" + randomNum1 + ".png";
 
-document.querySelectorAll("img")[0].setAttribute("src", randomImg);
+  document.querySelectorAll("img")[0].setAttribute("src", randomImg);
 
-var randomNum2 = Math.floor(Math.random() * 6) + 1;
-var randomImg2 = "images/dice" + randomNum2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
+  var randomNum2 = Math.floor(Math.random() * 6) + 1;
+  var randomImg2 = "images/dice" + randomNum2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImg2);
 
-if (randomNum1 > randomNum2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins!😉";
-} else if (randomNum1 < randomNum2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!😛";
-} else {
-  document.querySelector("h1").innerHTML = "It's a Draw!🤯";
+  if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!😉";
+  } else if (randomNum1 < randomNum2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!😛";
+  } else {
+    document.querySelector("h1").innerHTML = "It's a Draw!🤯";
+  }
+  document.querySelector("h2").style.visibility = "visible";
 }
